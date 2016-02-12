8087ff531aa5d09101ff0bdcd5d7f7f69597db44
Evgeniy <muse_uno@ukr.net>
2016-02-12 18:23:32 +0200

    commit new files status_pull_td

---
+++ deploy/gitbranch.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git branch',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Zhekan Git branch</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
